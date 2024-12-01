739e9a15c07ef11ac52251e14e76342c5859d638
jevaldev <jesusvalvir0354@gmail.com>
2024-11-30 18:38:07 -0600

    Prueba vercel

--- api/index.js
+++ api/index.js
@@ -5,30 +5,37 @@ import authRouter from '../controllers/auth.controller.js'
 import cookieParser from 'cookie-parser'
 
 const app = express()
+
+// Middleware
 app.use(cookieParser())
 app.use(json())
 
+// CORS configuration
 const allowedOrigins = ['http://localhost:8100', 'http://localhost:8101', 'http://localhost:5173']
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // Verificar si el origen está permitido
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('No permitido por CORS'))
-    }
-  },
-  methods: ['GET', 'POST', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Verificar si el origen está permitido
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('No permitido por CORS'))
+      }
+    },
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  })
+)
 
+// Rutas
 app.get('/', (req, res) => {
   res.status(200).send('Hello, world!')
 })
 
 app.use('/api/products', productsRouter)
 app.use('/api/auth', authRouter)
-// comentario
+
+// Exportar la app para Vercel
 export default app
