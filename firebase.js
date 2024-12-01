6101b95f9463937d79adba2564cb256046c06941
jevaldev <jesusvalvir0354@gmail.com>
2024-11-30 18:03:28 -0600

    Prueba vercel

--- firebase.js
+++ firebase.js
@@ -1,15 +1,15 @@
-import { initializeApp, cert } from 'firebase-admin/app'
-import { getFirestore } from 'firebase-admin/firestore'
-import { getStorage } from 'firebase-admin/storage'
-import serviceAccount from './creds.json' assert { type: 'json' }
+// import { initializeApp, cert } from 'firebase-admin/app'
+// import { getFirestore } from 'firebase-admin/firestore'
+// import { getStorage } from 'firebase-admin/storage'
+// import serviceAccount from './creds.json' assert { type: 'json' }
 
-// Initialize Firebase Admin SDK
-initializeApp({
-  credential: cert(serviceAccount),
-  storageBucket: 'gs://ecommercetiendita.appspot.com' // Asegúrate de reemplazar con tu bucket de almacenamiento
-})
+// // Initialize Firebase Admin SDK
+// initializeApp({
+//   credential: cert(serviceAccount),
+//   storageBucket: 'gs://ecommercetiendita.appspot.com' // Asegúrate de reemplazar con tu bucket de almacenamiento
+// })
 
-const db = getFirestore()
-const storage = getStorage().bucket() // Obtenemos la referencia al bucket de almacenamiento
+// const db = getFirestore()
+// const storage = getStorage().bucket() // Obtenemos la referencia al bucket de almacenamiento
 
-export { db, storage }
+// export { db, storage }
