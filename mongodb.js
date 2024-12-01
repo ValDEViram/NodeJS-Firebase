687c1a06a26829581f39d7eaf998b6685ff40d19
jevaldev <jesusvalvir0354@gmail.com>
2024-12-01 10:17:01 -0600

    Prueba vercel

--- mongodb.js
+++ mongodb.js
@@ -1,14 +1,19 @@
 import { MongoClient } from 'mongodb'
 import 'dotenv/config'
 
-const uri = process.env.MONGODB_URI // La URI completa con la base de datos incluida
-const client = new MongoClient(uri)
+const uri = process.env.MONGODB_URI
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true
+})
+
 let db
 
 export const connectDB = async () => {
   if (!db) {
     await client.connect()
-    db = client.db() // No necesitas pasar dbName, ya lo toma de la URI
+    db = client.db()
     console.log('Conectado a MongoDB')
   }
   return db
