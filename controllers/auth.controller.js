0e22a2d5125d5fcef37ecaf280d9fd4be0aab88b
jevaldev <jesusvalvir0354@gmail.com>
2024-11-30 18:09:13 -0600

    Prueba vercel

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,109 +1,109 @@
-// import { db } from '../firebase.js'
-// import { userRepository } from '../models/users.js'
-// import express from 'express'
-// import 'dotenv/config'
-// import jwt from 'jsonwebtoken'
+import { db } from '../firebase.js'
+import { userRepository } from '../models/users.js'
+import express from 'express'
+import 'dotenv/config'
+import jwt from 'jsonwebtoken'
 
-// const router = express.Router()
+const router = express.Router()
 
-// router.post('/register', async (req, res) => {
-//   const { email, username, password } = req.body
+router.post('/register', async (req, res) => {
+  const { email, username, password } = req.body
 
-//   // Validar datos antes de seguir
-//   if (!email || !username || !password) {
-//     return res
-//       .status(400)
-//       .json({ message: 'Todos los campos son obligatorios' })
-//   }
+  // Validar datos antes de seguir
+  if (!email || !username || !password) {
+    return res
+      .status(400)
+      .json({ message: 'Todos los campos son obligatorios' })
+  }
 
-//   // Llamar a la función que guarda el usuario en Firestore
-//   try {
-//     await userRepository.createUser({ email, username, password })
-//     res.status(201).json({ message: 'El registro fue exitoso', email })
-//   } catch (error) {
-//     res.status(500).json({ message: error.message || 'Error creando el usuario', error })
-//   }
-// })
+  // Llamar a la función que guarda el usuario en Firestore
+  try {
+    await userRepository.createUser({ email, username, password })
+    res.status(201).json({ message: 'El registro fue exitoso', email })
+  } catch (error) {
+    res.status(500).json({ message: error.message || 'Error creando el usuario', error })
+  }
+})
 
-// router.post('/sendVerification', async (req, res) => {
-//   const { email, code } = req.body
+router.post('/sendVerification', async (req, res) => {
+  const { email, code } = req.body
 
-//   if (!email || !code) throw new Error('Tanto el codigo como el correo son requeridos')
+  if (!email || !code) throw new Error('Tanto el codigo como el correo son requeridos')
 
-//   try {
-//     const userSnapshot = await db.collection('users')
-//       .where('email', '==', email)
-//       .where('regCode', '==', parseInt(code))
-//       .get()
+  try {
+    const userSnapshot = await db.collection('users')
+      .where('email', '==', email)
+      .where('regCode', '==', parseInt(code))
+      .get()
 
-//     if (userSnapshot.empty) {
-//       return res.status(400).json({ message: 'Codigo de verificación incorrecto' })
-//     }
+    if (userSnapshot.empty) {
+      return res.status(400).json({ message: 'Codigo de verificación incorrecto' })
+    }
 
-//     const userRef = userSnapshot.docs[0]?.ref
+    const userRef = userSnapshot.docs[0]?.ref
 
-//     if (!userRef) {
-//       return res.status(400).json({ message: 'No se encontró el usuario' })
-//     }
-//     await userRef.update({ validatedAccount: true, regCode: null })
+    if (!userRef) {
+      return res.status(400).json({ message: 'No se encontró el usuario' })
+    }
+    await userRef.update({ validatedAccount: true, regCode: null })
 
-//     res.status(200).json({ message: 'Cuenta verificada exitosamente' })
-//   } catch (error) {
-//     console.error('Error verificando cuenta:', error)
-//     res.status(500).json({ message: 'Error al verificar la cuenta' })
-//   }
-// })
+    res.status(200).json({ message: 'Cuenta verificada exitosamente' })
+  } catch (error) {
+    console.error('Error verificando cuenta:', error)
+    res.status(500).json({ message: 'Error al verificar la cuenta' })
+  }
+})
 
-// router.post('/login', async (req, res) => {
-//   const { email, password } = req.body
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body
 
-//   if (!email || !password) {
-//     return res
-//       .status(400)
-//       .json({ message: 'Todos los campos son obligatorios' })
-//   }
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ message: 'Todos los campos son obligatorios' })
+  }
 
-//   try {
-//     const { message, token } = await userRepository.loginUser({ email, password })
+  try {
+    const { message, token } = await userRepository.loginUser({ email, password })
 
-//     res.cookie('auth-token', token, {
-//       httpOnly: true,
-//       secure: process.env.NODE_ENV === 'production',
-//       maxAge: 3600000
-//     })
+    res.cookie('auth-token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 3600000
+    })
 
-//     res.status(200).json({ message })
-//   } catch (error) {
-//     res.status(400).json({ message: error.message || 'Error durante el inicio de sesión' })
-//   }
-// })
+    res.status(200).json({ message })
+  } catch (error) {
+    res.status(400).json({ message: error.message || 'Error durante el inicio de sesión' })
+  }
+})
 
-// router.get('/getUsers', async (req, res) => {
-//   const users = await userRepository.getUsers()
-//   try {
-//     res.status(201).send(users)
-//   } catch (error) {
-//     res.status(500).json({ message: 'Error al conseguir los usuarios', error })
-//   }
-// })
+router.get('/getUsers', async (req, res) => {
+  const users = await userRepository.getUsers()
+  try {
+    res.status(201).send(users)
+  } catch (error) {
+    res.status(500).json({ message: 'Error al conseguir los usuarios', error })
+  }
+})
 
-// router.get('/status', (req, res) => {
-//   const token = req.cookies['auth-token']
-//   if (!token) {
-//     return res.status(401).json({ authenticated: false })
-//   }
+router.get('/status', (req, res) => {
+  const token = req.cookies['auth-token']
+  if (!token) {
+    return res.status(401).json({ authenticated: false })
+  }
 
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET)
-//     res.status(200).json({ authenticated: true, user: decoded })
-//   } catch (error) {
-//     res.status(401).json({ authenticated: false })
-//   }
-// })
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    res.status(200).json({ authenticated: true, user: decoded })
+  } catch (error) {
+    res.status(401).json({ authenticated: false })
+  }
+})
 
-// router.post('/logout', (req, res) => {
-//   res.clearCookie('auth-token') // Limpia la cookie
-//   res.status(200).json({ message: 'Sesión cerrada' })
-// })
+router.post('/logout', (req, res) => {
+  res.clearCookie('auth-token') // Limpia la cookie
+  res.status(200).json({ message: 'Sesión cerrada' })
+})
 
-// export default router
+export default router
--- models/users.js
+++ models/users.js
@@ -1,235 +1,223 @@
-// import { hash, compare } from 'bcrypt'
-// import { db } from '../firebase.js'
-// import admin from 'firebase-admin'
-// import nodemailer from 'nodemailer'
-// import 'dotenv/config'
-// import jwt from 'jsonwebtoken'
-// import { productRepository } from './products.js'
+import { hash, compare } from 'bcrypt'
+import { connectDB } from '../mongodb.js'
+import nodemailer from 'nodemailer'
+import 'dotenv/config'
+import jwt from 'jsonwebtoken'
+import crypto from 'crypto'
+import { productRepository } from './products.js'
 
-// export class userRepository {
-//   static async createUser (userData) {
-//     try {
-//       const existingUserRef = await db.collection('users')
-//         .where('email', '==', userData.email)
-//         .get()
+export class userRepository {
+  static async createUser (userData) {
+    try {
+      const db = await connectDB()
+      const usersCollection = db.collection('users')
 
-//       // Si existe al menos un documento con ese correo
-//       if (!existingUserRef.empty) {
-//         throw new Error('El usuario ya existe con ese correo electrónico')
-//       }
+      // Verificar si el usuario ya existe
+      const existingUser = await usersCollection.findOne({ email: userData.email })
+      if (existingUser) {
+        throw new Error('El usuario ya existe con ese correo electrónico')
+      }
 
-//       const userRef = db.collection('users').doc()
-//       const hashPassword = await hash(userData.password, 10)
-//       const regCode = Math.floor(100000 + Math.random() * 9000)
+      const hashPassword = await hash(userData.password, 10)
+      const regCode = Math.floor(100000 + Math.random() * 9000)
 
-//       await this.sendVerificationEmail(userData.email, regCode)
+      await this.sendVerificationEmail(userData.email, regCode)
 
-//       await userRef.set({
-//         id: crypto.randomUUID(),
-//         email: userData.email,
-//         username: userData.username,
-//         password: hashPassword,
-//         regCode,
-//         validatedAccount: false,
-//         creationDate: admin.firestore.Timestamp.now()
-//       })
+      const user = {
+        id: crypto.randomUUID(),
+        email: userData.email,
+        username: userData.username,
+        password: hashPassword,
+        regCode,
+        validatedAccount: false,
+        creationDate: new Date()
+      }
 
-//       console.log('Usuario creado en Firestore con ID: ', userRef.id)
-//     } catch (error) {
-//       console.error('Error creando usuario en Firestore:', error)
-//       throw new Error(error.message || 'Error al crear el usuario') // Asegúrate de lanzar un mensaje específico
-//     }
-//   }
+      // Insertar el nuevo usuario en MongoDB
+      await usersCollection.insertOne(user)
 
-//   static async sendVerificationEmail (email, code) {
-//     const transporter = nodemailer.createTransport({
-//       host: 'smtp.sendgrid.net',
-//       port: 587,
-//       auth: {
-//         user: 'apikey',
-//         pass: process.env.SENDGRID_API_KEY
-//       }
-//     })
+      console.log('Usuario creado con ID: ', user.id)
+    } catch (error) {
+      console.error('Error creando usuario en MongoDB:', error)
+      throw new Error(error.message || 'Error al crear el usuario')
+    }
+  }
 
-//     const mailOptions = {
-//       from: process.env.EMAIL_USER,
-//       to: email,
-//       subject: 'Codigo de verificacion de cuenta',
-//       html: `
-//       <div style="padding: 20px; background-color: #f4f4f4;">
-//         <div style="max-width: 600px; margin: 0 auto; background-color: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
-//           <h2 style="color: #194860; text-align: center;">¡Bienvenido a SaboresBase!</h2>
-//           <p style="font-size: 16px;">Hola,</p>
-//           <p style="font-size: 16px;">Gracias por registrarte en nuestra plataforma. Para completar tu registro, por favor usa el siguiente código de verificación:</p>
-//           <div style="text-align: center; background-color: #f4f4f4; font-size: 24px; font-weight: bold; color: #194860; margin: 20px 0; border: 1px solid #13688b; box-shadow: 0px 5px 5px rgba(0,0,0, 0.1);">
-//             ${code}
-//           </div>
-//           <p style="font-size: 16px;">¡Esperamos que disfrutes de nuestra plataforma!</p>
-//           <p style="font-size: 16px; text-align: center;">El equipo de SaboresBase</p>
-//         </div>
-//       </div>
-//     `
-//     }
+  static async sendVerificationEmail (email, code) {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.sendgrid.net',
+      port: 587,
+      auth: {
+        user: 'apikey',
+        pass: process.env.SENDGRID_API_KEY
+      }
+    })
 
-//     try {
-//       await transporter.sendMail(mailOptions)
-//       console.log('Correo de verificación enviado')
-//     } catch (error) {
-//       console.error('Error enviando correo de verificación:', error)
-//       throw new Error('No se pudo enviar el correo de verificación')
-//     }
-//   }
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: 'Codigo de verificacion de cuenta',
+      html: `
+        <div style="padding: 20px; background-color: #f4f4f4;">
+          <div style="max-width: 600px; margin: 0 auto; background-color: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+            <h2 style="color: #194860; text-align: center;">¡Bienvenido a SaboresBase!</h2>
+            <p style="font-size: 16px;">Hola,</p>
+            <p style="font-size: 16px;">Gracias por registrarte en nuestra plataforma. Para completar tu registro, por favor usa el siguiente código de verificación:</p>
+            <div style="text-align: center; background-color: #f4f4f4; font-size: 24px; font-weight: bold; color: #194860; margin: 20px 0; border: 1px solid #13688b; box-shadow: 0px 5px 5px rgba(0,0,0, 0.1);">
+              ${code}
+            </div>
+            <p style="font-size: 16px;">¡Esperamos que disfrutes de nuestra plataforma!</p>
+            <p style="font-size: 16px; text-align: center;">El equipo de SaboresBase</p>
+          </div>
+        </div>
+      `
+    }
 
-//   static async loginUser (userData) {
-//     try {
-//       // Verificar si el usuario existe
-//       const existingUserRef = await db.collection('users')
-//         .where('email', '==', userData.email)
-//         .get()
+    try {
+      await transporter.sendMail(mailOptions)
+      console.log('Correo de verificación enviado')
+    } catch (error) {
+      console.error('Error enviando correo de verificación:', error)
+      throw new Error('No se pudo enviar el correo de verificación')
+    }
+  }
 
-//       if (existingUserRef.empty) {
-//         throw new Error('El correo no ha sido registrado')
-//       }
+  static async loginUser (userData) {
+    try {
+      const db = await connectDB()
+      const usersCollection = db.collection('users')
 
-//       const userDoc = existingUserRef.docs[0].data()
-//       const storedPassword = userDoc.password
+      // Verificar si el usuario existe
+      const user = await usersCollection.findOne({ email: userData.email })
+      if (!user) {
+        throw new Error('El correo no ha sido registrado')
+      }
 
-//       if (!userDoc.validatedAccount) {
-//         throw new Error('La cuenta no ha sido validada. Por favor, verifica tu correo electrónico.')
-//       }
-//       // Comparar la contraseña proporcionada con la almacenada
-//       const isPasswordValid = await compare(userData.password, storedPassword)
-//       if (!isPasswordValid) {
-//         throw new Error('Contraseña incorrecta')
-//       }
+      if (!user.validatedAccount) {
+        throw new Error('La cuenta no ha sido validada. Por favor, verifica tu correo electrónico.')
+      }
 
-//       const token = jwt.sign(
-//         { userID: userDoc.id, email: userDoc.email },
-//         process.env.JWT_SECRET,
-//         { expiresIn: '1h' }
-//       )
+      // Comparar la contraseña proporcionada con la almacenada
+      const isPasswordValid = await compare(userData.password, user.password)
+      if (!isPasswordValid) {
+        throw new Error('Contraseña incorrecta')
+      }
 
-//       return { message: 'Inicio de sesión exitoso', token }
-//     } catch (error) {
-//       console.error('Error durante el inicio de sesión:', error)
-//       throw new Error(error.message || 'Error al iniciar sesión')
-//     }
-//   }
+      const token = jwt.sign(
+        { userID: user.id, email: user.email },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+      )
 
-//   static async getUsers () {
-//     try {
-//       const usersRef = db.collection('users')
-//       const snapshot = await usersRef.get() // Obtiene todos los documentos
+      return { message: 'Inicio de sesión exitoso', token }
+    } catch (error) {
+      console.error('Error durante el inicio de sesión:', error)
+      throw new Error(error.message || 'Error al iniciar sesión')
+    }
+  }
 
-//       if (snapshot.empty) {
-//         console.log('No se encontraron usuarios')
-//         return []
-//       }
+  static async getUsers () {
+    try {
+      const db = await connectDB()
+      const usersCollection = db.collection('users')
 
-//       const users = []
-//       snapshot.forEach(doc => {
-//         users.push({ id: doc.id, ...doc.data() }) // Añade los datos de cada documento al array
-//       })
+      const users = await usersCollection.find().toArray()
+      if (users.length === 0) {
+        console.log('No se encontraron usuarios')
+        return []
+      }
 
-//       return users
-//     } catch (error) {
-//       console.error('Error obteniendo usuarios:', error)
-//       throw error
-//     }
-//   }
+      return users
+    } catch (error) {
+      console.error('Error obteniendo usuarios:', error)
+      throw error
+    }
+  }
 
-//   static async addProducts ({ userID, productos }) {
-//     try {
-//       const userQuerySnapshot = await db.collection('users')
-//         .where('id', '==', userID)
-//         .get() // Obtener la referencia al documento del usuario
+  static async addProducts ({ userID, productos }) {
+    try {
+      const db = await connectDB()
+      const usersCollection = db.collection('users')
 
-//       if (userQuerySnapshot.empty) {
-//         throw new Error('No existe el usuario')
-//       }
+      const user = await usersCollection.findOne({ id: userID })
+      if (!user) {
+        throw new Error('No existe el usuario')
+      }
 
-//       const userDoc = userQuerySnapshot.docs[0] // Obtener el primer documento
+      const updatedProducts = [...user.products]
 
-//       const existingProducts = userDoc.data().products || []
+      // Añadir productos al carrito
+      for (const product of productos) {
+        const existingProduct = await productRepository.getProductByID(product.productID)
+        if (existingProduct.stock < product.amount) {
+          throw new Error('Stock insuficiente para el producto')
+        }
 
-//       const updatedProducts = [...existingProducts]
-//       // async productos.forEach((product) => {
-//       for (const product of productos) {
-//         const existingProduct = await productRepository.getProductByID(product.productID)
-//         if (existingProduct.stock < product.amount) {
-//           throw new Error('Stock insuficiente para el producto')
-//         }
+        existingProduct.stock -= product.amount
+        await productRepository.editProduct(existingProduct)
 
-//         existingProduct.stock -= product.amount
-//         await productRepository.editProduct(existingProduct)
+        const index = updatedProducts.findIndex(p => p.productID === product.productID)
+        if (index !== -1) {
+          updatedProducts[index].amount += product.amount
+        } else {
+          updatedProducts.push(product)
+        }
+      }
 
-//         const index = existingProducts.findIndex(p => p.productID === product.productID)
-//         if (index !== -1) {
-//           updatedProducts[index].amount += product.amount
-//         } else {
-//           updatedProducts.push(product)
-//         }
-//       }
+      // Actualizar los productos del usuario
+      await usersCollection.updateOne({ id: userID }, { $set: { products: updatedProducts } })
 
-//       // Actualizar los productos del usuario
-//       await userDoc.ref.update({ products: updatedProducts })
+      console.log('Productos agregados al usuario: ', userID, '\n Productos: ', updatedProducts)
+    } catch (error) {
+      console.error('Error creando usuario en MongoDB:', error)
+      throw new Error(error.message || 'Error al agregar productos al usuario')
+    }
+  }
 
-//       console.log('Productos agregados al usuario: ', userID, '\n Productos: ', updatedProducts)
-//     } catch (error) {
-//       console.error('Error creando usuario en Firestore:', error)
-//       throw new Error(error.message || 'Error al crear el usuario')
-//     }
-//   }
+  static async getProductsInCart ({ ID }) {
+    try {
+      const db = await connectDB()
+      const usersCollection = db.collection('users')
 
-//   static async getProductsInCart ({ ID }) {
-//     try {
-//       const userQuerySnapshot = await db.collection('users')
-//         .where('id', '==', ID)
-//         .get()
-//       if (userQuerySnapshot.empty) {
-//         throw new Error('No existe el usuario')
-//       }
+      const user = await usersCollection.findOne({ id: ID })
+      if (!user) {
+        throw new Error('No existe el usuario')
+      }
 
-//       const userDoc = userQuerySnapshot.docs[0]
+      return user.products
+    } catch (error) {
+      console.log('Error al obtener los productos guardados', error)
+    }
+  }
 
-//       const userProducts = userDoc.data().products
-//       return userProducts
-//     } catch (error) {
-//       console.log('Erorr al obtener los productos guardados', error)
-//     }
-//   }
+  static listenToCartUpdates ({ ID }, onUpdate) {
+    const db = connectDB()
+    const usersCollection = db.collection('users')
 
-//   static listenToCartUpdates ({ ID }, onUpdate) {
-//     const userQuery = db.collection('users').where('id', '==', ID)
+    // Escuchar cambios en tiempo real (MongoDB Change Streams)
+    const changeStream = usersCollection.watch([{ $match: { 'fullDocument.id': ID } }])
 
-//     // Escuchar cambios en tiempo real en los productos del usuario
-//     const unsubscribe = userQuery.onSnapshot(async (snapshot) => {
-//       if (snapshot.empty) {
-//         console.error('No existe el usuario')
-//         return
-//       }
+    changeStream.on('change', async (change) => {
+      const updatedUser = change.fullDocument
+      const userProducts = updatedUser.products || []
 
-//       const userDoc = snapshot.docs[0]
-//       let userProducts = userDoc.data().products || []
+      // Actualizar datos de cada producto en el carrito
+      const updatedProducts = await Promise.all(
+        userProducts.map(async (product) => {
+          const storeProduct = await productRepository.getProductByID(product.productID)
+          return {
+            ...product,
+            productName: storeProduct.productName,
+            price: storeProduct.price,
+            imgName: storeProduct.imgName
+          }
+        })
+      )
 
-//       // Actualizar datos de cada producto en el carrito
-//       userProducts = await Promise.all(
-//         userProducts.map(async (product) => {
-//           const storeProduct = await productRepository.getProductByID(product.productID)
-//           return {
-//             ...product,
-//             productName: storeProduct.productName,
-//             price: storeProduct.price,
-//             imgName: storeProduct.imgName
-//           }
-//         })
-//       )
+      onUpdate(updatedProducts)
+    })
 
-//       // Llama a la función onUpdate con los datos actualizados
-//       onUpdate(userProducts)
-//     })
-
-//     // Devuelve la función para cancelar la suscripción si se necesita
-//     return unsubscribe
-//   }
-// }
+    // Devuelve la función para cancelar la suscripción si se necesita
+    return () => changeStream.close()
+  }
+}
